b1c9643b49bd987d6d68cc86c9d7386306c6a837
Rvjais <ranveerjais9984@gmail.com>
2026-08-04 11:35:27 +0530

    changed directory

--- backend/routes/calendar.route.js
+++
@@ -1,9 +0,0 @@
-import calendarController from "../controllers/calendar.controller.js";
-import { Router } from "express";
-import { authMiddleware } from "../middlewares/authenticate.js";
-
-const router = Router();
-
-router.get('/calendar-booking', authMiddleware, calendarController.getCalendar);
-
-export default router;
